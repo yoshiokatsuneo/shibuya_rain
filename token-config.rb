f376b9ed84d0d209435e75b3b7dd9116cfdd8555
Tsuneo Yoshioka <yoshiokatsuneo@gmail.com>
2012-12-18 00:34:29 +0200

    Initial commit

---
+++ token-config.rb
@@ -0,0 +1,6 @@
+CONSUMER_KEY       = 'xxxxxxxxxxxxxxxxxxx'
+CONSUMER_SECRET    = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+OAUTH_TOKEN        = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+OAUTH_TOKEN_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+
