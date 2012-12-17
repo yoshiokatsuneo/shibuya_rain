722f603c04a4cea20bd9e8a9c33a4303b1aef437
Tsuneo Yoshioka <yoshiokatsuneo@gmail.com>
2012-12-18 00:50:00 +0200

    Improvement...

--- shibuya_rain.rb
+++ shibuya_rain.rb
@@ -5,22 +5,24 @@ require 'twitter'
 require './token-config'
 
 Twitter.configure{|config|
-	config.consumer_key       = TWITTER_CONSUMER_KEY
-	config.consumer_secret    = TWITTER_CONSUMER_SECRET
-	config.oauth_token        = TWITTER_OAUTH_TOKEN
-	config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
+	config.consumer_key       = CONSUMER_KEY
+	config.consumer_secret    = CONSUMER_SECRET
+	config.oauth_token        = OAUTH_TOKEN
+	config.oauth_token_secret = OAUTH_TOKEN_SECRET
 }
 
 match_count = 0
-Twitter.search("rain", :count =>200, :result_type => "recent").results.map{|status|
-	# print "Tweet: #{Time.now - status.created_at}, #{status.from_user}: #{status.text}\n"
+keywords="渋谷,雨"
+threshold=3
+Twitter.search(keywords, :count =>200, :result_type => "recent").results.map{|status|
+	print "Tweet: #{Time.now - status.created_at}, #{status.from_user}: #{status.text}\n"
 	if(Time.now - status.created_at < 60 + 15) then
 		match_count += 1
 	end	
 }
 
-if match_count > 10 then
-	Twitter.update("There are #{match_count} tweets related to rain\n")
+if match_count >= threshold then
+	Twitter.update("「#{keywords}」でのツイートが１分間に#{threshold}個以上(#{match_count}個)ありました。\n")
 end
 
 
