99a3ba96df462ed5c03c989c416a89974e90c52d
Yoshioka Tsuneo <yoshiokatsuneo@gmail.com>
2015-11-04 15:47:44 +0900

    Updated for the latest Twitter gem

--- shibuya_rain.rb
+++ shibuya_rain.rb
@@ -4,21 +4,24 @@
 require 'twitter'
 require './token-config'
 
-Twitter.configure{|config|
+client = Twitter::REST::Client.new{|config|
 	config.consumer_key       = CONSUMER_KEY
 	config.consumer_secret    = CONSUMER_SECRET
-	config.oauth_token        = OAUTH_TOKEN
-	config.oauth_token_secret = OAUTH_TOKEN_SECRET
+	config.access_token        = OAUTH_TOKEN
+	config.access_token_secret = OAUTH_TOKEN_SECRET
 }
 
 match_count = 0
 keywords="渋谷,雨"
 threshold=3
-Twitter.search(keywords, :count =>200, :result_type => "recent").results.map{|status|
-	print "Tweet: #{Time.now - status.created_at}, #{status.from_user}: #{status.text}\n"
+client.search(keywords, :count =>200, :result_type => "recent").each{|status|
+	print "status=#{status}"
+	print "Tweet: #{Time.now - status.created_at}, #{status.user.screen_name}: #{status.text}\n"
 	if(Time.now - status.created_at < 60 + 15) then
 		match_count += 1
-	end	
+	else
+		break
+	end
 }
 
 if match_count >= threshold then
